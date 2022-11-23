8414b471a5d8dd4f5a7da76984a78e970f695c35
alexmedinasf <alexsales001@hotmail.com>
2022-11-23 08:53:42 -0300

    fetch missions from API

---
+++ src/utils/fetchMissionsFromAPI.js
@@ -0,0 +1,10 @@
+import { MISSIONS_URL } from './API_Data';
+
+const fetchMissionsFromAPI = async () => {
+  const response = await fetch(MISSIONS_URL);
+  const missions = await response.json();
+  return missions;
+};
+
+export default fetchMissionsFromAPI;
+    
